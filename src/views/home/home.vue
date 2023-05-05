bfd4a4bcb67b36c8c20dcaac1497f52fffe907da
Yun <905372421@qq.com>
2023-05-06 00:09:30 +0800

    feat✨: 1.初始化首页 2.抽取home-search组件

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,18 +4,14 @@
     <div class="banner">
       <img src="@/assets/img/home/banner.webp" alt="">
     </div>
-    <div class="location">
-      <div class="city">广州</div>
-      <div class="position">
-        <span class="text">我的位置</span>
-        <img src="@/assets/img/home/icon_location.png">
-      </div>
-    </div>
+    <home-search-box />
+   
   </div>
 </template>
 
 <script setup>
 import HomeNavBar from './cpns/home-nav-bar.vue'
+import HomeSearchBox from './cpns/home-search-box.vue'
 
 
 </script>
@@ -27,32 +23,5 @@ import HomeNavBar from './cpns/home-nav-bar.vue'
   }
 }
 
-.location {
-  display: flex;
-  align-items: center;
-  height: 44px;
-  padding: 0 20px;
 
-  .city {
-    flex: 1;
-  }
-
-  .position {
-    display: flex;
-    align-items: center;
-    width: 74px;
-
-    .text {
-      position: relative;
-      top: 2px;
-      font-size: 12px;
-    }
-
-    img {
-      margin-left: 5px;
-      width: 18px;
-      height: 18px;
-    }
-  }
-}
 </style>
