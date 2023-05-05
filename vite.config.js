3555963aa125fe86de661503cb27f355a9ab6119
Yun <905372421@qq.com>
2023-05-05 20:43:59 +0800

    初始化项目

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// vantUI -> 按需引入插件 (作用: 在打包的过程中检查引入的组件，如果有使用，会自动按需引入该组件)
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
