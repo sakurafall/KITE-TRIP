511df1ae5be9a03f2bf2908bf55baad8b4465cd4
Yun <905372421@qq.com>
2023-05-08 00:15:46 +0800

    feat✨: 完成首页页面的初步渲染

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,18 +5,50 @@
       <img src="@/assets/img/home/banner.webp" alt="">
     </div>
     <home-search-box />
-   
+    <home-categories />
+    <home-content />
+    <button @click="moreBtnClick">加载更多</button>
   </div>
 </template>
 
 <script setup>
+import useHomeStore from '@/stores/modules/home'
 import HomeNavBar from './cpns/home-nav-bar.vue'
 import HomeSearchBox from './cpns/home-search-box.vue'
+import HomeCategories from './cpns/home-categories.vue'
+import HomeContent from './cpns/home-content.vue'
+import { ref } from 'vue'
+
+// import { getHotSuggests } from '@/service'
+
+
+// 发送网络请求 页面中
+// 1. 热门建议
+// const hotSuggests = ref([])
+// getHotSuggests().then(res => {
+//   console.log(res)
+//   hotSuggests.value = res.data
+// })
+// 2. 分类列表
+// const categories = ref([])
+
+// 发送网络请求 store中
+const homeStore = useHomeStore()
+homeStore.fetchHotSuggestsData()
+homeStore.fetchCategoriesData()
+homeStore.fetchHouseListData()
+
+const moreBtnClick = () => {
+  homeStore.fetchHouseListData()
+}
 
 
 </script>
 
 <style lang="less" scoped>
+.home {
+  padding-bottom: 50px;
+}
 .banner {
   img {
     width: 100%;
