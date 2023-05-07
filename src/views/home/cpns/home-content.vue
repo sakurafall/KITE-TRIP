dd6c9b84fc4e997db3524814a898f88e05d61255
Yun <905372421@qq.com>
2023-05-08 00:08:09 +0800

    feat✨: 首页热门精选模块：抽取为单独组件，且根据不同类型再次抽取为小组件

---
+++ src/views/home/cpns/home-content.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="content">
+    <div class="title">热门精选</div>
+    <div class="list">
+      <template v-for="item in houseList" :key="item.data.houseId">
+        <house-item-v3 v-if="item.discoveryContentType === 3" :item-data="item.data" />
+        <house-item-v9 v-else-if="item.discoveryContentType === 9" :item-data="item.data" />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import useHomeStore from "@/stores/modules/home";
+import HouseItemV3 from '@/components/house-item-v3/house-item-v3.vue';
+import HouseItemV9 from '@/components/house-item-v9/house-item-v9.vue';
+
+
+const homeStore = useHomeStore()
+const { houseList } = storeToRefs(homeStore)
+</script>
+
+<style lang="less" scoped>
+
+.content {
+  padding: 10px;
+
+  .title {
+    font-size: 22px;
+    padding: 10px;
+  }
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+
+</style>
