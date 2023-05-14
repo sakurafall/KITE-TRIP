5de715682b9dab7c03e3a63a778819d71b4763ff
Yun <905372421@qq.com>
2023-05-14 22:36:14 +0800

    feat✨: 搜索栏的封装和使用

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -6,18 +6,24 @@
     </div>
     <home-search-box />
     <home-categories />
+    <div v-if="isShowSearchBar" class="search-bar">
+      <search-bar class="" />
+    </div>
     <home-content />
-    <button @click="moreBtnClick">加载更多</button>
   </div>
 </template>
 
 <script setup>
+import { computed, ref, watch } from 'vue'
+
 import useHomeStore from '@/stores/modules/home'
 import HomeNavBar from './cpns/home-nav-bar.vue'
 import HomeSearchBox from './cpns/home-search-box.vue'
 import HomeCategories from './cpns/home-categories.vue'
 import HomeContent from './cpns/home-content.vue'
-import { ref } from 'vue'
+import SearchBar from '@/components/search-bar/search-bar.vue'
+
+import useScroll from '@/hooks/useScroll.js'
 
 // import { getHotSuggests } from '@/service'
 
@@ -38,9 +44,29 @@ homeStore.fetchHotSuggestsData()
 homeStore.fetchCategoriesData()
 homeStore.fetchHouseListData()
 
-const moreBtnClick = () => {
-  homeStore.fetchHouseListData()
-}
+// 监听window窗口的滚动
+// 1. 当我们离开页面时，我们移除监听
+// 2. 如果别的页面也进行类似的监听，会编写重复代码
+// useScroll(() => {
+//   homeStore.fetchHouseListData()
+// })
+const { isReachBottom, scrollTop } = useScroll()
+watch(isReachBottom, (newValue) => { // 监听到标识符变化的时候，执行js逻辑
+  if (newValue) {
+    homeStore.fetchHouseListData().then(() => {
+      isReachBottom.value = false
+    })
+  }
+})
+
+// 搜索框显示的控制
+// const isShowSearchBar = ref(false)
+// watch(scrollTop, (newTop) => {
+//   isShowSearchBar.value = newTop > 100
+// })
+const isShowSearchBar = computed(() => { // 定义的响应式数据, 依赖另外一个可响应式的数据
+  return scrollTop.value >= 360
+})
 
 
 </script>
@@ -49,11 +75,21 @@ const moreBtnClick = () => {
 .home {
   padding-bottom: 50px;
 }
+
 .banner {
   img {
     width: 100%;
   }
 }
 
-
+.search-bar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 45px;
+  padding: 16px 16px 10px;
+  background: white;
+  z-index: 9;
+}
 </style>
